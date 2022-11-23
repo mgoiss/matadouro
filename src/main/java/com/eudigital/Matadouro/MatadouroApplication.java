af42db377ab495edd20529c5cdb6902f8e67af8b
Matheus Gois <mrochagois@gmail.com>
2022-11-22 22:36:18 -0300

    Criação do projeto e ambinete test

---
+++ src/main/java/com/eudigital/Matadouro/MatadouroApplication.java
@@ -0,0 +1,13 @@
+package com.eudigital.Matadouro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatadouroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatadouroApplication.class, args);
+	}
+
+}
